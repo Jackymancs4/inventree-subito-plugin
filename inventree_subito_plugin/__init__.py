d27d4a558461ad7b841703abd2ebce86a93ea68b
Giacomo Rossetto <jackyman_cs4@live.it>
2024-07-13 12:43:53 +0200

    Cut new release Various improvements

---
+++ inventree_subito_plugin/__init__.py
@@ -0,0 +1,3 @@
+from .subito import SubitoPlugin
+
+__all__ = ["SubitoPlugin"]
--- inventree_subito_plugin/subito.py
+++ inventree_subito_plugin/subito.py
@@ -42,6 +42,11 @@ class SubitoPlugin(ActionMixin, APICallMixin, SettingsMixin, InvenTreePlugin):
     }
 
     def import_image(self, url: str, part: PartAttachment) -> bool:
+        """
+        Download an image given it's URL, and attach it to the part.
+        Would be cool to attach it to the SupplierPart, but it's not really a 
+        thing for now.
+        """
 
         if part.attachment:
             return False
@@ -67,21 +72,30 @@ class SubitoPlugin(ActionMixin, APICallMixin, SettingsMixin, InvenTreePlugin):
         return False
 
     def import_image_async(self, url, part):
+        """
+        Async version of the same method.
+        """
+
         offload_task(self.import_image, url, part)
 
-    def import_supplier_part(self, supplier_id, subito_list_id: str, part_id: str):
-        print("Importing supplier part  " + subito_list_id)
+    def import_supplier_part(self, supplier_id, part_id: str, subito_list_id: str):
+        """
+        Add a supplier part
+        """
+        
+        logger.info("Importing supplier part " + subito_list_id)
 
         url = "v1/search/items?list_ids=" + str(subito_list_id)
 
         response = self.api_call(endpoint=url)
 
-        supplier_part_expired = False
+        supplier_part_retired = False
 
+        # If there are no ads, than the add has been retired.
         if len(response['ads']) > 0:
             supplier_part_data = response['ads'][0]
         else: 
-            supplier_part_expired = True
+            supplier_part_retired = True
 
         part = Part.objects.get(pk=part_id)
 
@@ -93,45 +107,49 @@ class SubitoPlugin(ActionMixin, APICallMixin, SettingsMixin, InvenTreePlugin):
             SKU=subito_list_id,
         )[0]
 
-        supplier_part.description = supplier_part_data['subject']
-        supplier_part.note = supplier_part_data['body']
-        supplier_part.link = supplier_part_data['urls']['default']
-        supplier_part.available = 1
+        if supplier_part_retired: 
+            supplier_part.active=False,
+            supplier_part.update_available_quantity(0)
+            supplier_part.save()
+        else: 
+            supplier_part.active=True,
+            supplier_part.description = supplier_part_data['subject']
+            supplier_part.note = supplier_part_data['body']
+            supplier_part.link = supplier_part_data['urls']['default']
+            supplier_part.update_available_quantity(1)
 
-        # Save the whole object for good measure
-        supplier_part.metadata['subito'] = supplier_part_data
-        supplier_part.save()
+            # Save the whole object for good measure
+            supplier_part.metadata['subito'] = supplier_part_data
+            supplier_part.save()
 
-        # Add images as part attachments
-        for image in supplier_part_data['images']:
+            # Add images as part attachments
+            for image in supplier_part_data['images']:
 
-            image_url = image['scale'][4]['uri']
+                image_url = image['scale'][4]['uri']
 
-            part_attachment = PartAttachment.objects.get_or_create(
-                part=part,
-                link=image_url,
-                comment=image['uri'] 
-            )[0]
+                part_attachment = PartAttachment.objects.get_or_create(
+                    part=part,
+                    link=image_url,
+                    comment=image['uri'] 
+                )[0]
 
-            self.import_image(image_url, part_attachment)
+                self.import_image(image_url, part_attachment)
 
-        # Add price break
+            # Add price break
+            price = "0.0"
+            for feature in supplier_part_data['features']:
+                if feature['uri'] == '/price':
+                    price = feature['values'][0]['key']
 
-        price = "0.0"
-        for feature in supplier_part_data['features']:
-            if feature['uri'] == '/price':
-                price = feature['values'][0]['key']
+            if(price != "0.0") :
 
-        if(price != "0.0") :
+                supplier_part_price = SupplierPriceBreak.objects.get_or_create(
+                    part=supplier_part,
+                    quantity=1,
+                )[0]
 
-            supplier_part_price = SupplierPriceBreak.objects.get_or_create(
-                part=supplier_part,
-                quantity=1,
-            )[0]
-
-            supplier_part_price.price = price
-            supplier_part_price.save()
-
+                supplier_part_price.price = price
+                supplier_part_price.save()
 
     @property
     def api_url(self):
@@ -149,7 +167,17 @@ class SubitoPlugin(ActionMixin, APICallMixin, SettingsMixin, InvenTreePlugin):
             part_id = data.get("part_id")
             subito_list_id = data.get("subito_list_id")
 
-            self.import_supplier_part(supplier_id, subito_list_id, part_id)
+            self.import_supplier_part(supplier_id, part_id, subito_list_id)
+
+        if command == "update_supplier_parts":
+
+            supplier_parts = SupplierPart.objects.filter(metadata__icontains="spoolman_id")
+
+            for supplier_part in supplier_parts:
+                logger.debug("Supplier part found: " + str(supplier_part.pk))
+
+                self.import_supplier_part(supplier_id, supplier_part.part.pk, supplier_part.SKU)
+
 
     def get_info(self, user, data=None):
         """Sample method."""
